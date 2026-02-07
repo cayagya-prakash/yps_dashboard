b7a8d3bf847978d8a9a09e6b97dff335d761cd01
Yukti Mehta <mehtayukti317@gmail.com>
2026-02-07 17:22:02 +0530

    Add layout file

---
+++ app/admin/dashboard/blogs/edit/layout.jsx
@@ -0,0 +1,14 @@
+
+import React from 'react'
+
+function BlogEcitLayout({ children }) {
+    return (
+        <div className='container'>
+
+            <div>{children}</div>
+
+        </div>
+    )
+}
+
+export default BlogEcitLayout
