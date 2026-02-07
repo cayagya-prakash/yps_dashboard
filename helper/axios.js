b3ed24bf6eb042a6361d027f74d7a0aada96925c
Yukti Mehta <mehtayukti317@gmail.com>
2026-02-07 14:26:28 +0530

    Add files

--- helper/axios.js
+++ helper/axios.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 
-const BASE_URL = "http://localhost:5000/api";
+const BASE_URL = process.env.BASE_URL;
+// const BASE_URL = "http://localhost:5000/api";
 // const BASE_URL = import.meta.env.VITE_API_BASE_URL ;
 
 const service = axios.create({
