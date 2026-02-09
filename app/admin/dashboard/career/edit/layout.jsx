08c59a25116644d6d248ea8e1ca3b323df348643
Yukti Mehta <mehtayukti317@gmail.com>
2026-02-09 12:05:01 +0530

    Add layout file in career

---
+++ app/admin/dashboard/career/edit/layout.jsx
@@ -0,0 +1,14 @@
+
+import React from 'react'
+
+function CareerEditLayout({ children }) {
+    return (
+        <div className='container'>
+
+            <div>{children}</div>
+
+        </div>
+    )
+}
+
+export default CareerEditLayout
